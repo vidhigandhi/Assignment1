6e289b661d931bef7e596169dcedcca6f3e3483a
Vidhi - Gandhi <vgandhi@linprog4.cs.fsu.edu>
2017-10-08 15:18:58 -0400

    Added and implemented class SimpleAxe.h

---
+++ SimpleAxe.h
@@ -0,0 +1,30 @@
+/*
+ * File: SimpleAex.h
+ * Author: Vidhi <vg15e@my.fsu.edu>
+ *
+ * Created on October 8, 2017, 3:11 PM
+ */
+
+#include <string>
+
+#ifndef SIMPLEAE_H
+#define SIMPLEAXE_H
+
+/* Defines the specification of a simple axe where:
+ *	 0 <= armor < 20 -- ignore all armor points 
+ * 	 Total possible hitpoints = 60 
+ */
+
+class SimpleAxe : public Weapon {
+
+	public: 
+		SimpleAxe() : Weapon ("Simple Axe", 60) {}
+		virtual ~SimpleAxe() {};
+		virtual double hit(double armor);
+
+}
+
+#endif 
+
+
+
