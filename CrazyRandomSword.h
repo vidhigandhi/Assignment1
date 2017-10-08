cea4679f7c3ac82c5d188cb7ed0a0798d90d368f
Vidhi - Gandhi <vgandhi@linprog4.cs.fsu.edu>
2017-10-08 15:48:21 -0400

    Implemented class CrazyRandomSword.h

---
+++ CrazyRandomSword.h
@@ -0,0 +1,29 @@
+/*
+ * File: CrazyRandomSword.h
+ * Author: Vidhi <vg15e@my.fsu.edu>
+ *
+ * Created on October 8, 2017, 3:37 PM
+ */
+
+#include <string>
+#include <cstdlib> 	/* rand() */
+#include "Weapon.h"
+
+#ifndef CRAZYRANDOMSWORD_H
+#define CRAZYRANDOMSWORD_H
+
+/* Defines the behavior of a crazy random sword where the class generates a random number 
+   between 10 and 100.
+	Hitpoints = random number between 10 and 100; ignores int armor points ranging from 
+	0 to half of the armor the weapon hit
+*/
+
+class CrazyRandomSword : public Weapon 
+{
+	public:
+ 		CrazyRandomSword() : Weapon ("Crazy Random Sword", std::rand()% 100 + 10) {}
+		virtual ~CrazyRandomSword() {};
+		virtual double hit (double armor);
+};
+
+#endif
