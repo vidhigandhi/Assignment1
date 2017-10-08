1426175ae84adbb423b618f9f6ce690c3ca7e5ae
Vidhi - Gandhi <vgandhi@linprog4.cs.fsu.edu>
2017-10-08 16:46:29 -0400

    PocketKnife.cpp added

---
+++ PocketKnife.cpp
@@ -0,0 +1,22 @@
+/*
+ * File: PocketKnife.cpp
+ * Author: Vidhi <vg15e@my.fsu.edu>
+ *
+ */
+
+#include "PocketKnife.h"
+
+double PocketKnife::hit (double armor)
+{
+	double damage;
+	if ((int)armor > 29)
+		damage = hitpoints;
+	else if ((int)armor <=29 )
+		damage = 0.15 * armor;
+
+	//return value
+	if (damage < 0) 
+		return 0;
+
+	return damage; 
+}
